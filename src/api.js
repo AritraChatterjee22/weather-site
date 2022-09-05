d2f6906f38bb239096d9905198adc14f4e6bfff9
Aritra Chatterjee <marlboro@pop-os.localdomain>
2022-09-05 20:15:54 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0638e5f66bmsh19eb5915cbc4edap1e84a4jsn72035562db25',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d86762ce01a8679960298792b6b24aba";
+
+
